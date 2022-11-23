6cc1ea651656e36a120c3fb5772dc26da88d27ac
adam <adam7973@edu.campusvejle.dk>
2022-11-23 13:34:29 +0100

    0027_files as rust

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+
 mod morris;
 
 fn main() {
+
+}
+
+fn s0027_files() {
+    let mydata = "Hi,\nthis is a text file.\nIt has 3 rows.\n";
+    let myfile = Path::new("input01.txt");
+    let mut file = match fs::File::create(&myfile) {
+        Err(_) => panic!("couldn't create file"),
+        Ok(file) => file,
+    };
+
+    match file.write_all(mydata.as_bytes()) {
+        Err(_) => panic!("err"),
+        Ok(_) => println!("wrote to file"),
+    }
+}
+
+fn s0030_morris() {
     let mut me = morris::Morris::new();
     for _i in 1..1001 {
         if me.sleepiness > 50 {
             me.sleep()
         } else if me.thirst > 50 {
-            me.buy_whisky();
             me.drink();
         } else if me.hunger > 50 {
             me.eat();
+        } else if me.whisky == 0 && me.gold > 0 {
+            me.buy_whisky();
         } else {
             me.mine();
         }
     }
     println!("{}", me)
 }
-
